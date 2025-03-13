e6e42de04f322d790d7eb8ecc44b264fdfd2e0cd
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2025-03-13 13:22:15 +0100

    ...

--- Pregunta1.R
+++ Pregunta1.R
@@ -1,5 +1,86 @@
 #Solucion Pregunta 1
 
+x <- c(0,1)
+fx <- c(0.68, 0.32)
+
+#tabla de prob
+cbind(x, fx)
+
+plot(x, fx, pch=16, col="red", ylim=c(0,1))
+lines(x, fx, type="h", col="red")
+
+mu <- sum(x*fx)
+mu
+
+sigmasq <- sum((x-mu)^2*fx) 
+sigmasq
+
+0.68*0.32
+
+x
+fx
+
+n <- 43
+Y <- function(i){sum(sample(x, n, prob = fx, replace=TRUE))}
+
+#bucle
+m <- 400000
+muestra <- sapply(1:m, Y)
+fi <- table(muestra)/m
+fi
+
+#frecuencias relativas 
+data.frame(fi, Fi=cumsum(fi))
+
+barplot(fi)
+
+###
+
+#tabla de prob  
+data.frame(Y=0:43, 
+           Prob=dbinom(0:43, 43, 0.32))
+
+y <- 0:43
+fy <- dbinom(y, 43, 0.32)
+
+plot(y, fy, pch=19, col="red")
+lines(y, fy, col="red", type="h")
+
+
+dbinom(13, 43, 0.32) #respuesta
+
+############
+
+#tabla de prob  
+y <- 0:44
+Pi <- dbinom(y, 44, 0.32)
+Fi <- cumsum(df$Prob)
+
+pbinom(16, 44, 0.32)
+cbind(y, fi, Fi)
+
+plot(y, Fi, type="s", col="red")
+
+####
+
+x <- 0:24
+Pi <- dbinom(x, 24, 0.68)
+
+mu <- sum(x*Pi)
+24*0.68
+
+sum((x-mu)^2*Pi)
+24*0.68*0.32
+
+Fi <- cumsum(Pi)
+plot(x, Fi, type="s", col="red")
+
+qbinom(0.25, 24, 0.68)
+
+
+############################
+#Solucion Pregunta 1
+
 #ensayo de Bernoulli
 
 x <- c(0, 1)
